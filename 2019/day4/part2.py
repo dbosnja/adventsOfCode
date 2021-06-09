f1d0d7938938d90080b19bf44da70447f7e132fc
dbosnja <dombosnjak@gmail.com>
2021-06-09 20:00:56 +0200

    day4 solved

--- 2019/day4/part2.py
+++ 2019/day4/part2.py
@@ -12,27 +12,15 @@ def main(password_range):
 
 
 def is_ascending(pass_cand):
-    temp_digit = pass_cand % 10
-
-    while pass_cand:
-        pass_cand //= 10
-        if pass_cand % 10 > temp_digit:
-            return False
-        temp_digit = pass_cand % 10
-
+    pass_cand = str(pass_cand)
+    if any(pass_cand[i] > pass_cand[i+1] for i in range(len(pass_cand) - 1)):
+        return False
     return True
 
+
 def contains_siblings(pass_cand):
-    return any([x[0] == x[1] for x in slices([i for i in str(pass_cand)])])
-
-
-def slices(a_list):
-    temp_list = []
-    for i, _ in enumerate(a_list):
-        if i == len(a_list) - 1:
-            break
-        temp_list.append(a_list[i:i + 2])
-    return temp_list
+    groups = set(str(pass_cand))
+    return any(str(pass_cand).count(ch) == 2 for ch in groups)
 
 
 if __name__ == "__main__":
