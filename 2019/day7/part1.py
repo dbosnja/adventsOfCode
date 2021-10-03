8e346c8bf5bc0e27bbb008f2d940fa2b152a1f3c
dbosnja <dombosnjak@gmail.com>
2021-10-03 12:58:52 +0200

    day7 part1 - initial commit

---
+++ 2019/day7/part1.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3.9
+# https://adventofcode.com/2019/day/7 - part 1
+
+
+def get_input(file_path):
+    pass
+
+
+def main():
+    file_path = "input.txt"
+
+
+if __name__ == "__main__":
+    print(f"{main()}")
