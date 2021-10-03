925975102434181aee32596f831a56a465acfad4
dbosnja <dombosnjak@gmail.com>
2021-10-03 14:25:37 +0200

    day7 part1 solved

--- 2019/day7/part1.py
+++ 2019/day7/part1.py
@@ -1,13 +1,27 @@
 #! /usr/bin/python3.9
 # https://adventofcode.com/2019/day/7 - part 1
 
+from itertools import permutations
+
+import op_code
+
 
 def get_input(file_path):
-    pass
+    with open(file_path) as f:
+        return [int(i) for i in f.read().strip().split(",")]
 
 
 def main():
     file_path = "input.txt"
+    instruction_set = get_input(file_path)
+    thruster_values = []
+    for p in permutations(range(5)):
+        a_input = 0
+        for phase in p:
+            a_output = op_code.main(phase, a_input, list(instruction_set))
+            a_input = a_output
+        thruster_values.append(a_output)
+    return max(thruster_values)
 
 
 if __name__ == "__main__":
